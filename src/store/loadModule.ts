f33cf009b2611b8373b6dce06f9ef9c9c5d4a48f
m.kosmas <m.kosmas@ergologic.gr>
2021-12-24 11:23:48 +0200

    helpers

--- src/store/loadModule.ts
+++ src/store/loadModule.ts
@@ -1,8 +1,41 @@
-export interface Module = {
+import { Request as RequestInterface, Route } from '../helpers'
 
+/**
+ * A reducer holds application/module global state information.
+ * You can read more about reducers and redux [here](https://redux.js.org/).
+ */
+export interface Reducer {
+  [reducerName: string]: {
+    /**
+     * Default reducer state
+     */
+    initialState: Object
+    handlers: {
+      [actionType: string]: (state: Object, action: Object) => typeof state
+    }
+  }
 }
 
-const _modules = []
+export interface ApiHandler {
+  [actionType: string]: RequestInterface
+}
+
+export interface Actions {
+  [actionType: string]: RequestInterface
+}
+
+export interface Module {
+  name: string
+  modules?: Array<Module>
+  actions?: Object
+  types?: Object
+  reducer?: Reducer
+  api?: ApiHandler
+  locales?: Object
+  routes?: Object
+}
+
+const _modules: Array<Module> = []
 let _router = {}
 let _locales = {}
 let _api = {}
@@ -36,8 +69,8 @@ const Module = {
       ...reducers
     }
   },
-  loadRoutes: (modules = _modules) => {
-    const routes = modules.reduce((prv, curr) => {
+  loadRoutes: (modules = _modules): Array<Route> => {
+    const routes: Object = modules.reduce((prv, curr) => {
       const { routes, modules } = curr
       return {
         ...prv,
@@ -47,7 +80,7 @@ const Module = {
     }, _router)
     return Object.keys(routes).map((key) => routes[key])
   },
-  getTranslations: (modules = _modules) => {
+  getTranslations: (modules = _modules): Object => {
     return modules.reduce((prv, curr) => {
       const { name, locales, modules } = curr
       if (!locales) return prv
@@ -58,7 +91,7 @@ const Module = {
       }
     }, _locales)
   },
-  getRequestHandlers: (modules = _modules) => {
+  getRequestHandlers: (modules = _modules): Object => {
     return modules.reduce((prv, curr) => {
       const { name, api, modules } = curr
       if (!api) return prv
@@ -69,7 +102,7 @@ const Module = {
       }
     }, _api)
   },
-  loadReducers: (modules = _modules) => {
+  loadReducers: (modules = _modules): Object => {
     return modules.reduce((prv, curr) => {
       const { name, reducer, modules } = curr
       if (!reducer) return prv
